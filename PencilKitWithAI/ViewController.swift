1ab69b0288d2fb51078491be5660d9efa69e9ab3
COMI <wincomi@me.com>
2022-12-02 22:11:15 +0900

    first commit

---
+++ PencilKitWithAI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PencilKitWithAI
+//
+//  Created by COMI on 2022/12/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
